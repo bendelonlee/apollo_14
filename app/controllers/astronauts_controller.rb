fbb2857d94b0a81ae7e9a3540cba8686b20ff7da
Ben Lee <bendelonlee@gmail.com>
2018-12-07 10:36:29 -0700

    Implement through user story 2

--- app/controllers/astronauts_controller.rb
+++ app/controllers/astronauts_controller.rb
@@ -1,5 +1,6 @@
 class AstronautsController < ApplicationController
   def index
+    @average_age = Astronaut.average_age
     @astronauts = Astronaut.all
   end
 end
--- spec/features/user_sees_all_astronauts_spec.rb
+++ spec/features/user_sees_all_astronauts_spec.rb
@@ -2,12 +2,11 @@ describe 'user sees all astronauts' do
   describe 'user visits astronaut index page' do
     before(:each) do
       @astronaut_1 = Astronaut.create(name: "Joe", age: 40, job: "Specialist")
-      @astronaut_2 = Astronaut.create(name: "Lisa", age: 41, job: "Chief")
-      @astronaut_3 = Astronaut.create(name: "Alex", age: 21, job: "Makes coffee")
-
+      @astronaut_2 = Astronaut.create(name: "Lisa", age: 50, job: "Chief")
+      @astronaut_3 = Astronaut.create(name: "Alex", age: 30, job: "Makes coffee")
+      visit '/astronauts'
     end
     it 'displays a list of all astronauts' do
-      visit '/astronauts'
       expect(page).to have_content(@astronaut_1.name)
       expect(page).to have_content(@astronaut_1.job)
       expect(page).to have_content(@astronaut_1.age)
@@ -19,6 +18,10 @@ describe 'user sees all astronauts' do
       expect(page).to have_content(@astronaut_3.name)
       expect(page).to have_content(@astronaut_3.job)
       expect(page).to have_content(@astronaut_3.age)
+
+    end
+    it 'displays the average age of the astronauts' do
+      expect(page).to have_content("Average age: 40")
 
     end
   end
